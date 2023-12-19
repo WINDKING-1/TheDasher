e09b65a02ebdcf069d53e6af3858996e7056d51e
WINDKING-1 <zainosamamohamed@outlook.com>
2023-12-19 22:00:02 +0200

    Beta
    added movement, removed files.

--- The dasher code.py
+++ The dasher code.py
@@ -14,7 +14,10 @@ Ground_surf=pygame.image.load(r'e1.png').convert()
 
 player_surf=pygame.image.load(r'e3.png').convert_alpha()
 player_rect=player_surf.get_rect(midbottom=(800,350))
-player_movement=3
+player_movement=5
+playerright=pygame.transform.flip(player_surf,True,False)
+playerleft=pygame.transform.flip(playerright,True,False)
+left=False
 
 while True:
     for event in pygame.event.get():
@@ -25,12 +28,32 @@ while True:
     screen.blit(Sky_surf,(0,0))
     screen.blit(Ground_surf,(0,350))
     screen.blit(player_surf,player_rect)
+    keys = pygame.key.get_pressed()
 
-    player_rect.x=player_rect.x-player_movement
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= player_movement
+        left=True
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += player_movement
+        left=False
+    if keys[pygame.K_UP]:
+        player_rect.y -= player_movement
+    if keys[pygame.K_DOWN]:
+        player_rect.y += player_movement
 
-    if player_rect.x<=-5 or player_rect.x>=760:
-        player_movement=player_movement*-1
-        player_surf=pygame.transform.flip(player_surf,True,False)
+    if player_rect.x<=-40:
+        player_rect.x=760
+    if player_rect.x>=780:
+        player_rect.x=-20    
+    if left:
+        player_surf = playerleft
+    else:
+        player_surf = playerright
+    if player_rect.y > 259:
+        player_rect.y = 259
+    
+    
+        
     
     pygame.display.update()
     fps.tick(60)
