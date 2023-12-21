aeb534e9ca18b43d52900a3710ae687dbe7e4d83
WINDKING-1 <zainosamamohamed@outlook.com>
2023-12-21 18:50:02 +0200

    fixed jump and  smoothed out

--- The dasher code.py
+++ The dasher code.py
@@ -5,7 +5,6 @@
 import pygame
 from sys import exit
 
-
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("The Dasher v0.01")
@@ -14,19 +13,20 @@ pygame.display.set_icon(icon)
 Text_font = pygame.font.Font(r'font/Pixeltype.ttf',50)
 fps=pygame.time.Clock()
 
+level=1
 
-#Sky_surf=pygame.image.load(r'sky.jpg').convert()
 Sky_surf=pygame.image.load(r'bettersky.png').convert()
 Ground_surf=pygame.image.load(r'download.png').convert()
 Ground_rect=Ground_surf.get_rect(bottomleft=(0,400))
-
+Sky_surf2=pygame.image.load(r'sky.jpg').convert()
+Ground_surf2=pygame.image.load(r'e4.png').convert()
 
 player_surf=pygame.image.load(r'e3.png').convert_alpha()
 player_rect=player_surf.get_rect(midbottom=(700,352))
 player_movement=5
 player_fallseed=8
-player_jump=10
-player_aircount = 5
+player_jump=5
+player_aircount = -8
 
 dashavailabe=True
 dash_d=120
@@ -43,32 +43,54 @@ def playergoright():
 def playergoleft():
     player_rect.x -= player_movement
 
+def player_y_min():
+    if player_rect.y > 259:
+        player_rect.y = 259
+
+def disdancecap():
+    if player_rect.x<=-51:
+        player_rect.x=775
+    if player_rect.x>=780:
+        player_rect.x=-50 
+
+def gravity():
+    player_rect.y += player_fallseed
+
+def lvl1():
+    screen.blit(Sky_surf,(0,0))
+    screen.blit(Ground_surf,(0,351))
+    screen.blit(player_surf,player_rect)
+
+def lvl2():
+    screen.blit(Sky_surf2,(-570,-500))
+    screen.blit(Ground_surf2,(0,351))
+    screen.blit(player_surf,player_rect)
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
 
-    #screen.blit(Sky_surf,(-570,-500))
-    screen.blit(Sky_surf,(0,0))
-    screen.blit(Ground_surf,(0,351))
-    screen.blit(player_surf,player_rect)
-
+    if level==1:
+        lvl1()
+    elif level==2:
+        lvl2()
     keys = pygame.key.get_pressed()
 
     if player_rect.colliderect(Ground_rect):
         airborn=False
-        player_aircount=6
+        player_aircount=0
     else:
         airborn=True
         
-    if player_aircount == 6:
-        player_rect.y += player_fallseed
+    if player_aircount == 0 and airborn:
+        gravity()
 
-    if player_aircount < 6:
-        player_rect.y -= player_aircount * player_jump
+    if player_aircount < 0:
+        player_rect.y += player_aircount * player_jump
 
-    if player_aircount !=6:
+    if player_aircount !=0:
         player_aircount+=1
     
     if not dashavailabe and (pygame.time.get_ticks()-last_dashtimer)>dash_cd:
@@ -77,18 +99,18 @@ while True:
     if keys[pygame.K_a]:
         playergoleft()
         left=True
+
     if keys[pygame.K_d]:
         playergoright()
         left=False
+
     if keys[pygame.K_SPACE] and not airborn:
         player_rect.y -= player_jump
-        player_aircount = 1
-
+        player_aircount = -7
 
     if keys[pygame.K_s]:
         for i in range(-3,0):
             player_rect.y-=player_fallseed*i
-
 
     if dashavailabe:
         if keys[pygame.K_LSHIFT]:
@@ -100,22 +122,19 @@ while True:
                 player_rect.x-=dash_d
                 dashavailabe=False
                 last_dashtimer=pygame.time.get_ticks()
+            
             if not left:
                 player_rect.x+=dash_d
                 dashavailabe=False
                 last_dashtimer=pygame.time.get_ticks()
 
-    if player_rect.x<=-51:
-        player_rect.x=775
-    if player_rect.x>=780:
-        player_rect.x=-50    
+    disdancecap()
     if left:
         player_surf = playerleft
     else:
         player_surf = playerright
-    if player_rect.y > 259:
-        player_rect.y = 259
-        
-    
+
+    player_y_min()
+ 
     pygame.display.update()
     fps.tick(60)
