8996585bcfb9704a7d4b10b4140afbdf60f46587
WINDKING-1 <zainosamamohamed@outlook.com>
2023-12-23 03:35:22 +0200

    0.4

--- The dasher code.py
+++ The dasher code.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("The Dasher v0.01")
 icon = pygame.image.load(r"thedasher.png")
 pygame.display.set_icon(icon)
-Text_font = pygame.font.Font(r'font/Pixeltype.ttf',50)
+Text_font = pygame.font.Font(r'font/Pixeltype.ttf',35)
 fps=pygame.time.Clock()
 
 level=1
@@ -33,9 +33,16 @@ dash_d=120
 dash_cd=600
 last_dashtimer=0
 
+combo_count=0
+combo_font_size=35
+combo_font = pygame.font.Font(r'font/Pixeltype.ttf',combo_font_size)
+
 playerright=pygame.transform.flip(player_surf,True,False)
 playerleft=pygame.transform.flip(playerright,True,False)
 left=True
+
+player_health=70
+Health_text= str(player_health)+"%"
 
 def playergoright():
     player_rect.x += player_movement
@@ -72,6 +79,47 @@ def dash_up():
 def dash_down():
     player_rect.y += dash_d
 
+def dash_left():
+    player_rect.x-=dash_d
+
+def dash_right():
+    player_rect.x+=dash_d
+
+def Health_min():
+    global player_health
+    if player_health<0:
+        player_health=0
+
+def status_text():
+    if player_health<=10:
+        Health_color='purple'
+
+    elif player_health<=30:
+        Health_color='red'
+
+    elif player_health<=60:
+        Health_color='orange'
+
+    elif player_health<=80:
+        Health_color='yellow'
+
+    else:
+        Health_color='green'
+    
+    Health_surf=Text_font.render(Health_text, True,Health_color)
+    screen.blit(Health_surf,(20,20))
+
+def combo_coloring():
+    if combo_count==0:
+        combo_color='white'
+    elif combo_count==1:
+        combo_color='yellow'
+        combo_font_size=55
+        
+    combo='x'+str(combo_count)
+    Combo_surf=combo_font.render(combo, True,combo_color)
+    screen.blit(Combo_surf,(20,40))
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -103,11 +151,11 @@ while True:
     if not dashavailabe and (pygame.time.get_ticks()-last_dashtimer)>dash_cd:
         dashavailabe=True
 
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         playergoleft()
         left=True
 
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         playergoright()
         left=False
 
@@ -115,7 +163,7 @@ while True:
         player_rect.y -= player_jump
         player_aircount = -9
 
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         for i in range(-3,0):
             player_rect.y-=player_fallseed*i
 
@@ -128,23 +176,25 @@ while True:
                 dash_down()
 
             if left:
-                player_rect.x-=dash_d
+                dash_left()
                 dashavailabe=False
                 last_dashtimer=pygame.time.get_ticks()
             
             if not left:
-                player_rect.x+=dash_d
+                dash_right()
                 dashavailabe=False
                 last_dashtimer=pygame.time.get_ticks()
-
-    distancecap()
 
     if left:
         player_surf = playerleft
     else:
         player_surf = playerright
 
+    distancecap()
     player_y_min()
- 
+    status_text()
+    combo_coloring()
+    Health_min()
+
     pygame.display.update()
     fps.tick(60)
